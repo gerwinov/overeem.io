917b2bb684c5caddbc4ea3e8bbda068c20b90090
Gerwin Overeem <gerwin@foryard.nl>
2018-09-17 11:10:01 +0200

    Fixed image locations

--- src/assets/data/experience.js
+++ src/assets/data/experience.js
@@ -4,7 +4,7 @@ export const experience = [
     subtitle: 'Frontend developer',
     description: `Foryard is een software innovatie lab met als doel haar klanten te helpen innoveren met behulp van software.
                   Ik heb hier gewerkt aan verschillende prototypes, mvp's en producten, met name in de rol van frontend developer.`,
-    image: `/img/logos/foryard.png`,
+    image: `img/logos/foryard.png`,
     startYear: 2017,
     endYear: 2018,
     color: '#57ea8a'
@@ -19,7 +19,7 @@ export const experience = [
                   Ook was ik bij Ebicus onderdeel van het high-potential programma. <br><br>
                   Zie <a href="https://www.linkedin.com/in/gerwinovereem" rel="noreferrer nofollow">LinkedIn</a> voor een
                   compleet overzicht van al mijn werkervaring bij Ebicus`,
-    image: `/img/logos/ebicus.png`,
+    image: `img/logos/ebicus.png`,
     startYear: 2010,
     endYear: 2017,
     color: '#007cc2'
@@ -30,7 +30,7 @@ export const experience = [
     description: `(Via Ebicus) - Bij Interpolis heb ik als lead developer meegewerkt aan een upgrade van Siebel. Hierbij was ik o.a.
                   verantwoordelijk voor het organiseren van workshops met keyusers om de gebruikerservaring van Siebel te verbeteren
                   d.m.v. aanpassingen in het front-end framework van Siebel (Open UI).`,
-    image: `/img/logos/interpolis.png`,
+    image: `img/logos/interpolis.png`,
     startYear: 2017,
     endYear: 2017,
     color: '#0099a1'
@@ -41,7 +41,7 @@ export const experience = [
     description: `(Via Ebicus) - Siebel is één van de core-applicaties binnen de IND. Als developer heb ik meegewerkt aan het verder
                   uitbouwen van de functionaliteit; o.a. door integraties met andere overheidspartijen en ondersteunen
                   van nieuwe bedrijfsprocessen in Siebel. Ook heb ik bij de IND ervaring opgedaan als teamlead en solution analist.`,
-    image: `/img/logos/ind.png`,
+    image: `img/logos/ind.png`,
     startYear: 2014,
     endYear: 2017,
     color: '#42145f'
@@ -51,7 +51,7 @@ export const experience = [
     subtitle: 'Master - Information Studies (cum laude)',
     description: `<strong>Specialisatie:</strong> Business Information Systems <br>
                   <strong>Masterscriptie:</strong> “Effects of SaaS on competencies of brokerages in IT” - cijfer: 9`,
-    image: `/img/logos/uva.png`,
+    image: `img/logos/uva.png`,
     startYear: 2011,
     endYear: 2013,
     color: '#000000'
@@ -61,7 +61,7 @@ export const experience = [
     subtitle: 'Bachelor - Information Studies',
     description: `<strong>Specialisatie:</strong> Informatie, Multimedia, Management <br>
                   <strong>Bachelorproject:</strong> “Data gathering and combining for improved quality of advice for NSure, Wageningen” - cijfer: 7,5`,
-    image: `/img/logos/vu.png`,
+    image: `img/logos/vu.png`,
     startYear: 2007,
     endYear: 2010,
     color: '#0089cf'
