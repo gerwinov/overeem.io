c0e24b53a2abb2ef486530232eb8dc9cd0601209
Gerwin Overeem <gerwin@foryard.nl>
2018-09-18 09:47:52 +0200

    Removed unused dependency from build

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,13 @@ module.exports = {
     themeColor: '#f7b93a',
     msTileColor: '#ffffff'
   },
+  configureWebpack: {
+    resolve: {
+      alias: {
+        'chart.js': 'chart.js/dist/Chart.js'
+      }
+    }
+  },
   baseUrl: process.env.NODE_ENV === 'production'
     ? '/overeem.io/'
     : '/'
